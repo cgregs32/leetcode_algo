60886d69e1c8bb4fa93861b740bd78478acf9c86
Chris Gregory <cgregs32@gmail.com>
2019-02-20 09:55:44 -0700

    add_binary

---
+++ add_binary.rb
@@ -0,0 +1,7 @@
+def add_binary(a, b)
+  (a.to_i(2) + b.to_i(2)).to_s(2)
+end
+
+a = "11" 
+b = "1"
+p add_binary(a, b)
---
+++ battle_ship.rb
@@ -0,0 +1,7 @@
+# WIP
+def count_battleships(board)
+  board.map { |b| p b}
+end
+
+board = [["X", ".", ".", "X"], [".", ".", ".", "X"], [".", ".", ".", "X"]]
+p count_battleships(board)
